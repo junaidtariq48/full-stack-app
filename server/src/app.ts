e855706bd2c90617fdb8f81288e18cf6e069be93
Junaid Tariq <junaid.tariq48@gmail.com>
2023-12-23 12:09:11 +0400

    fix server file

--- server/src/app.ts
+++ server/src/app.ts
@@ -31,8 +31,7 @@ app.use("/items", itemRoutes);
 
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-
+  app.use(express.static(path.join(__dirname, "../../client/build")));
   app.get("*", (req, res) =>
     res.sendFile(
       path.resolve(__dirname, "../", "client", "build", "index.html")
