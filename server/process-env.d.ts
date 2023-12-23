3078550775eb71da91ba14b27120af8dd8da152d
Junaid Tariq <junaid.tariq48@gmail.com>
2023-12-23 12:15:13 +0400

    added new env variable

--- server/process-env.d.ts
+++ server/process-env.d.ts
@@ -1,12 +1,13 @@
 export {};
 
 declare global {
-    namespace NodeJS {
-      interface ProcessEnv {
-        [key: string]: string | undefined;
-        PORT: string;
-        DB_URL: string | undefined;
-        DB_NAME: string | undefined;
-      }
+  namespace NodeJS {
+    interface ProcessEnv {
+      [key: string]: string | undefined;
+      PORT: string;
+      DB_URL: string | undefined;
+      DB_NAME: string | undefined;
+      API_URL: string | undefined;
     }
   }
+}
