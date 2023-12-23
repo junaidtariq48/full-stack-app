0e0365670f46cda52a82ae564865fbc2d6b96379
Junaid Tariq <junaid.tariq48@gmail.com>
2023-12-23 11:51:26 +0400

    server file fix for client

--- server/src/app.ts
+++ server/src/app.ts
@@ -31,10 +31,12 @@ app.use("/items", itemRoutes);
 
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../", "client", "dist", "index.html"))
+    res.sendFile(
+      path.resolve(__dirname, "../", "client", "build", "index.html")
+    )
   );
 } else {
   app.get("/", (req, res) => res.send("Please set to production"));
